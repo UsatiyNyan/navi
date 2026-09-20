5733dc3a5d6b567f194c1f5c93ccdd82b4d82413
UsatiyNyan <us4tiyny4n@gmail.com>
2026-09-21 00:34:40 +0200

    flake: wayland, plugins

--- flake.nix
+++ flake.nix
@@ -22,11 +22,35 @@
         extraBuildInputs = with pkgs; [
           just
           lld
+
+          # graphics
+          wayland
+          wayland-scanner
+          wayland-protocols
+          libxkbcommon
+          libffi
+          libglvnd
+          vulkan-loader
+          vulkan-headers
         ];
         extraShellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib:${pkgs.vulkan-loader}/lib:/run/opengl-driver/lib:$LD_LIBRARY_PATH"
+          export XDG_DATA_DIRS="/run/opengl-driver/share:$XDG_DATA_DIRS"
         '';
         extraNixvimComponents = [
           ({...}: {
+            plugins = {
+              lsp.servers.glsl_analyzer.enable = true;
+              lsp.servers.wgsl_analyzer.enable = true;
+
+              treesitter.settings.ensure_installed = [
+                "glsl"
+                "wgsl"
+                "fbs"
+              ];
+
+              # lsp.servers.rust_analyzer.settings.cargo.target = "wasm32-unknown-unknown";
+            };
           })
         ];
       };
